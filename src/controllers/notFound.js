1daf2be446580da7d8525767e6a63d14e035696d
vitassablino <105976325+vitassablino@users.noreply.github.com>
2023-10-26 23:44:39 +0300

    Revert "init pr"

--- src/controllers/notFound.js
+++
@@ -1,6 +0,0 @@
-const NotFoundError = require('../errors/notFoundError'); //
-
-/* Обработчик страницы 404 */
-module.exports.notFound = (req, res, next) => {
-  next(new NotFoundError("Запрашиваемая страница не найдена или не существует"));
-};
